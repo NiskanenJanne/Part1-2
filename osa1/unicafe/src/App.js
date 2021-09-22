7d7264ca6c5cee4e1c6e4519de05bc3ccdb92c4c
Janne Niskanen <janne.niskanen@aalto.fi>
2021-09-22 19:27:26 +0300

    1.9

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Statistics = (props) => {
+  const all = () => props.good + props.neutral + props.bad
+  console.log(all())
+  const average = () => (1 * props.good + 0 * props.neutral + (-1) * props.bad) / all()
+  console.log(average())
+  const pos = () => props.good / all()
+  console.log(pos())
+  if (props.good, props.neutral, props.bad === 0){
+    return(
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+  return(
+    <div>
+      <>good {props.good}<br/></>
+      <>neutral {props.neutral}<br/></>
+      <>bad {props.bad}<br/></>
+      <>all {all()}<br/></>
+      <>average {average()}<br/></>
+      <>pos {pos()}</>
     </div>
-  );
+  )
 }
+
+
+
+
+const App = () => {
+  const Header = "Give feedback"
+  const statistics = "statistics"
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return(
+    <div>
+      <h1>{Header}</h1>
+
+        <button onClick={()=> setGood(good + 1)}>
+          good
+        </button>
+        <button onClick={()=> setNeutral(neutral + 1)}>
+          neutral
+        </button>
+        <button onClick={()=> setBad(bad + 1)}>
+          bad
+        </button>
+
+      <h1>{statistics}</h1>
+
+      <div>
+        <Statistics good={good}
+        neutral={neutral}
+        bad={bad}/>
+      </div>
+    </div>
+  )
+}
+
+
+
 
 export default App;
